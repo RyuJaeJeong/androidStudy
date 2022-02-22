ba8fd61e6a048bfe09f10901e92e1bcc44c80310
RyuJaeJeong <fb0982@naver.com>
2022-02-22 22:45:40 +0900

    리사이클러뷰 실습

--- Ch1102/app/src/main/java/com/example/ch11_02/MainActivity.kt
+++ Ch1102/app/src/main/java/com/example/ch11_02/MainActivity.kt
@@ -3,18 +3,46 @@ package com.example.ch11_02
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
-import com.example.ch11_02.databinding.ActivityMainBinding
-import com.example.ch11_02.databinding.LayoutBinding
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ch11_02.databinding.ItemMainBinding
+import com.example.ch11_02.databinding.RecyclerviewBinding
 
-class MainActivity : AppCompatActivity() {
+class MyViewHolder(val binding: ItemMainBinding): RecyclerView.ViewHolder(binding.root)
+
+class MyAdapter(val binding: ItemMainBinding): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun getItemCount(): Int = datas.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder = MyViewHolder(
+        ItemMainBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        Log.d("kkang", "onBindViewHolder : $position")
+        val binding = (holder as MyViewHolder).binding
+        binding.itemData.text = datas[position]
+        binding.itemRoot.setOnClickListener{
+            Log.d("kkang", "item root click : $position")
+        }
+    }
+}
+
+class RecyclerViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val binding = LayoutBinding.inflate(layoutInflater)
-        setSupportActionBar(binding.toolbar)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        val binding = RecyclerviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val datas = mutableListOf<String>()
+        for (i in 1..10) {
+            datas.add("Item $i")
+        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = MyAdapter(datas)
+        binding.recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayout Manager.VERTICAL))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         //  <!--android:parentActivityName=".MainActivity"--> 혹은 메니 페스트파일에서 이를 선언해주는 방법도 있음.
     }
